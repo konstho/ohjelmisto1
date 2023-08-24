a680737a51c0492c60df47b8b11f8c245cde2bd4
konstho <142784554+konstho@users.noreply.github.com>
2023-08-24 21:02:27 +0300

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,6 @@
+kanta = float(input("syötä suorakulmion kanta: "))
+korkeus = float(input("syötä suorakulmion korkeus: "))
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+print("suorakulmion piiri on: ", piiri)
+print("suorakulmion pinta-ala on:", pinta_ala)
