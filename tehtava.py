13bbe535059270230960061d74a4681e4b4242e9
konstho <142784554+konstho@users.noreply.github.com>
2023-08-24 20:59:47 +0300

    Delete tehtava.py

--- tehtava.py
+++
@@ -1,6 +0,0 @@
-kanta = float(input("syötä suorakulmion kanta: "))
-korkeus = float(input("syötä suorakulmion korkeus: "))
-piiri = 2 * (kanta + korkeus)
-pinta_ala = kanta * korkeus
-print("suorakulmion piiri on: ", piiri)
-print("suorakulmion pinta-ala on:", pinta_ala)
