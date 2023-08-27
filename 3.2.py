a240e2ddd9b284310151b2af3301e7151dfcaf70
konstho <142784554+konstho@users.noreply.github.com>
2023-08-27 12:55:32 +0300

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,11 @@
+hytti = input("anna hytin kirjain: ")
+if hytti == "lux":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hytti == "a":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hytti == "b":
+    print("B on hytti autokannen yläpuolella.")
+elif hytti == "c":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("virheellinen hyttiluokka.")
